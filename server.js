93846c0bf6ad9f1e0ab6703d801df20eadf0f685
Gerald Jay Lagarto <geraldjaydlagarto@gmail.com>
2020-10-19 19:54:57 +0800

    Export user routes.

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express()
 const bodyParser = require('body-parser')
 const {db} = require('./db/index')
 const organization = require('./routes/organization.route')
+const user = require('./routes/user.route')
 const port = 3000;
 
 db()
 app.use(bodyParser.urlencoded( {extended: true}))
 app.use(bodyParser.json({}))
 app.use(organization)
+app.use(user)
 
 app.listen(port, () => {
     console.log('Server runs at port:', port)
