7109f74566a1938f2114b47d96f60df384cab344
Gerald Jay Lagarto <geraldjaydlagarto@gmail.com>
2020-10-19 12:28:27 +0800

    Add insert function to organization controller.

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -11,3 +11,42 @@ const GetAllOrganization = async (req, res) => {
         console.log('error', error)
     }
 }
+
+const AddOrganization = async (req, res) => {
+    try {
+        const {
+            org_name,
+            org_description,
+            org_country,
+            org_city,
+            org_picture,
+        } = req.body
+        const existing_organization = await OrganizationService.FindOne({
+            org_name
+        })
+
+        if(existing_organization){
+            return res.status(409).json({
+                message: 'Data exists',
+            })
+        }
+
+        await OrganizationService.Create({
+            org_name,
+            org_description,
+            org_country,
+            org_city,
+            org_picture,
+        })
+
+        return res.status(200).json({
+            message: 'Data inserted',
+        })
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+
+module.exports = {
+    GetAllOrganization
+}
