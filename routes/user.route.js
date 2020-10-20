82f8667f22eca0d5f67f138599fc702174e4b9e5
Gerald Jay Lagarto <geraldjaydlagarto@gmail.com>
2020-10-20 15:28:26 +0800

    Add get users by type and user data by id in route. Am using the topdown approach.

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const UserController = require('../controllers/user.controller')
 
 router.get('/users', UserController.GetAllUsers)
+router.get('/users/:user_type', UserController.GetUsersByType)
+router.get('/user/:user_id', UserController.GetUserById)
 router.post('/user', UserController.AddUser)
 router.put('/user/:user_id', UserController.UpdateUser)
 router.delete('/user/:user_id', UserController.DeleteUser)
