b4c146cab634e2d0711d1f7135f54bffe9937dca
Gerald Jay Lagarto <geraldjaydlagarto@gmail.com>
2020-10-20 15:18:46 +0800

    Install eslint.

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({
-    user_last_name: String,
-    user_first_name: String,
-})
+const UserSchema = new mongoose.Schema(
+    {
+        username: String,
+        name: String,
+        email: String,
+        password: String,
+        country: String,
+        userType: {
+            type: String,
+            enum: ['user','ngo_admin','super_admin'],
+            default: 'user'
+        },
+    },
+    {
+        versionKey: false
+    }
+)
 
 const User = mongoose.model('user', UserSchema, 'user')
 module.exports = User
